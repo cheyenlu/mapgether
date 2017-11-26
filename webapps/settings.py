5ddf04b1bb66891b55e744882b397f02655a75b0
akshaybhagat <akster.91@gmail.com>
2017-11-25 21:01:16 -0500

    add mmlslave.ddns.net to whitelist

--- webapps/settings.py
+++ webapps/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mapgether.us-east-1.elasticbeanstalk.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['mapgether.us-east-1.elasticbeanstalk.com', '127.0.0.1', 'localhost', 'mmlslave.ddns.net']
 
 
 # Application definition
